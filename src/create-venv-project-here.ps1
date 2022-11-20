373c4e9e67dd1ff67960befe9c97c44f9cbd2fd9
Brady Trudel <bradytrudel@outlook.com>
2022-11-20 17:02:02 -0500

    DAD

--- src/create-venv-project-here.ps1
+++ src/create-venv-project-here.ps1
@@ -1,14 +1,23 @@
 python --version
 
-Write-Host "Creating a virtual environment  ..."
+Write-Host "Creating a Python 3 virtual environment for Windows ..."
 py -3 -m venv .venv
 
 Write-Host "Activating virtual environment  ..."
 Start-Process -FilePath '.\.venv\Scripts\activate.bat' -NoNewWindow
 
-Write-Host "Creating a requirements.txt and main.py file ..."
-Out-File ".\main.py" -Encoding utf8
-pip freeze > requirements.txt
+$mainpy = ".\main.py"
+$requirements = ".\requirements.txt"
+
+if (-not(Test-Path -Path $mainpy -PathType Leaf)) {
+    Write-Host "Creating a default $mainpy file ..."
+    Out-File $mainpy -Encoding utf8
+}
+
+if (-not(Test-Path -Path $requirements -PathType Leaf)) {
+    Write-Host "Creating a default $requirements file using pip ..."
+    pip freeze > requirements.txt
+}
 
 Write-Host "Opening VSCode ..."
 code .
